2043bc27544291d319b9f12ab86b3fd30c230f29
Heang Ly <leechingheang87@gmail.com>
2023-11-26 15:05:18 +0000

    test form code server

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.example {
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
     requires transitive javafx.graphics;
-
+// test
 
     opens com.example to javafx.fxml;
     exports com.example;
