ad426d7178955b7aa4a6f49c982430b394ee6173
Heang Ly <ly@onlyheang.com>
2023-11-26 15:11:23 +0000

    try again

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example {
     requires com.fasterxml.jackson.databind;
     requires transitive javafx.graphics;
 // test
+//try
 
     opens com.example to javafx.fxml;
     exports com.example;
